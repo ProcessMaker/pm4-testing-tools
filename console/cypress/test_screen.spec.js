a8d12f7d9cf220e15538e56dfda78ac5c05e85f6
David Callizaya <davidcallizaya@gmail.com>
2021-12-17 11:19:41 -0400

    Add hints for screen tests

--- console/cypress/test_screen.spec.js
+++ console/cypress/test_screen.spec.js
@@ -4,9 +4,15 @@ describe('{testName}', () => {
     cy.visit('/');
   });
 
-  it('Verify $TEST_NAME', () => {
+  it('Verify {testName}', () => {
     cy.loadFromJson('{testScreenFile}', 0);
+    // set init screen test data
+    // cy.setPreviewDataInput({person: []});
     cy.get('[data-cy=mode-preview]').click();
+
+    // cy.get('[data-cy=preview-content] [name="form_input_1"]').should('not.be.visible');
+    // cy.get('[data-cy=preview-content] [name=form_checkbox_1]').click();
+    // cy.get('[data-cy=preview-content] [name="select1.form_input_1"]').clear().type('it works');
 
     // Check the data of the screen
     cy.assertPreviewData({
